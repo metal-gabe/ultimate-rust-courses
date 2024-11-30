439ec0033f617491ad1eece4060e3206df81b567
Metal Gabe <gfl.wrk@gmail.com>
2024-11-29 20:17:33 -0800

    chore(main): prettify & add some syntactic sugar to exercise G;

--- exercise/g_collections_enums/src/main.rs
+++ exercise/g_collections_enums/src/main.rs
@@ -17,7 +17,7 @@ enum Shot {
 
 impl Shot {
     // Here is a method for the `Shot` enum you just defined.
-    fn points(&self) -> i32 {
+    fn points(self) -> i32 {
         // 1b. Implement this method to convert a Shot into points
         // - return 5 points if `self` is a `Shot::Bullseye`
         // - return 2 points if `self` is a `Shot::Hit(x)` where x < 3.0
@@ -25,7 +25,7 @@ impl Shot {
         // - return 0 points if `self` is a Miss
         match self {
             Shot::Bullseye => 5,
-            Shot::Hit(x) if *x < 3.0 => 2,
+            Shot::Hit(x) if x < 3.0 => 2,
             Shot::Hit(x) => 1,
             Shot::Miss => 0,
         }
@@ -47,22 +47,22 @@ fn main() {
     //      - Greater than 5.0 -- `Shot::Miss`
     for coord in arrow_coords {
         coord.print_description();
-        let distance = coord.distance_from_center();
 
-        if distance < 1.0 {
-            shots.push(Shot::Bullseye);
-        } else if distance < 5.0 {
-            shots.push(Shot::Hit(distance));
-        } else {
-            shots.push(Shot::Miss);
-        }
+        let shot = match coord.distance_from_center() {
+          x if x < 1.0 => Shot::Bullseye,
+          x if x < 5.0 => Shot::Hit(x),
+          _ => Shot::Miss,
+        };
+
+        shots.push(shot);
     }
 
     let mut total = 0;
     // 3. Finally, loop through each shot in shots and add its points to total
     for shot in shots {
-        println!("Shot points: {}", shot.points());
-        total += shot.points();
+        let points: i32 = shot.points();
+        total += points;
+        println!("Shot points: {}", points);
     }
 
     println!("Final point total is: {total}");
