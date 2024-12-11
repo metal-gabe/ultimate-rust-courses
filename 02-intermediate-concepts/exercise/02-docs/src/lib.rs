333c48e3afbd935297b5ce397d0672fc4b6527fa
Metal Gabe <gfl.wrk@gmail.com>
2024-12-10 20:21:14 -0800

    chore(main): complete exercise 2;

--- 02-intermediate-concepts/exercise/02-docs/src/lib.rs
+++ 02-intermediate-concepts/exercise/02-docs/src/lib.rs
@@ -15,21 +15,41 @@
 // - Document the "roundness" field, explaining that it is a percentage
 // - Document the "orangeness" field, explaining that it is a number from 8 to 27
 
+/*!
+A pumpkin is a cultivated winter squash in the genus Cucurbita.
+The term is most commonly applied to round, orange-colored squash varieties,
+but does not possess a scientific definition.
+It may be used in reference to many different squashes of varied appearance
+and belonging to multiple species in the Cucurbita genus.
+
+![A Pumpkin, oui, oui](https://upload.wikimedia.org/wikipedia/commons/thumb/5/5c/FrenchMarketPumpkinsB.jpg/700px-FrenchMarketPumpkinsB.jpg)
+
+!*/
+
+/// A pumpkin is a winter squash with large orange seeds.
+///
+/// # Recipes
+/// Coming soon...
 pub struct Pumpkin {
-    pub roundness: f32,
-    pub orangeness: i32,
+   /// The range, from 8 to 27, of how orange a pumpkin is.
+   pub orangeness: i32,
+   /// The percentage of how round a pumpkin is.
+   pub roundness: f32,
 }
 
 // 4. Document the "smash" method. Explain that if you smash the pumpkin, it will be gone. Then it
 // can't be used for pie. :'-(
 
 impl Pumpkin {
-    pub fn smash(self) {}
+   /// If you smash a pumpkin, it will be gone.
+   /// Then it can't be used for pie. :'-(
+   pub fn smash(self) {}
 }
 
 // 5. Document that BURNT_ORANGE is for the "orangeness" field in the Pumpkin struct.
 // - Link to the Pumpkin struct in your description
-
+/// This is used for the "orangeness" field in the Pumpkin struct
+/// Clickable link: [`Pumpkin`]
 pub const BURNT_ORANGE: i32 = 13;
 
 // Challenge: Find the option to pass to `cargo doc` so that documentation for this private item
@@ -39,8 +59,8 @@ pub const BURNT_ORANGE: i32 = 13;
 /// At least not by default. But if you pass the correct option in, it will magically appear!
 #[allow(dead_code)] // to silence the warning
 enum PrivateEnum {
-    /// For Halloween. To be lit by candlelight.
-    JackOLantern,
-    /// For dessert during North American winter holidays.
-    PumpkinPie,
+   /// For Halloween. To be lit by candlelight.
+   JackOLantern,
+   /// For dessert during North American winter holidays.
+   PumpkinPie,
 }
