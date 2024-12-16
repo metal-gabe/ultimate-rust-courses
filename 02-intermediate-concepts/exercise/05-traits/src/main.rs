14d941263112ff91927682a306fad9b596eaf4b6
Metal Gabe <gfl.wrk@gmail.com>
2024-12-16 15:06:48 -0800

    chore(main): move `Party` & `Cake` stuff (exercise 5 intermediate concepts) to `lib.rs`;

--- 02-intermediate-concepts/exercise/05-traits/src/main.rs
+++ 02-intermediate-concepts/exercise/05-traits/src/main.rs
@@ -1,46 +1,4 @@
-use std::cmp::PartialEq;
-
-#[derive(Clone, Copy, Debug, PartialEq)]
-pub enum Cake {
-   Chocolate,
-   MapleBacon,
-   Spice,
-}
-
-impl From<&Party> for Cake {
-   fn from(party: &Party) -> Self {
-      party.cake.clone()
-   }
-}
-
-// deriving `PartialEq` here for `Party` means that all properties must be the same
-// using the manually implemented `PartialEq` below means we can customize & specify the comparison
-#[derive(Debug)] // #[derive(Debug, PartialEq)]
-pub struct Party {
-   pub at_restaurant: bool,
-   pub num_people: u8,
-   pub cake: Cake,
-}
-
-impl Default for Party {
-   fn default() -> Self {
-      Party {
-         at_restaurant: true,
-         cake: Cake::Chocolate,
-         num_people: 8,
-      }
-   }
-}
-
-impl PartialEq for Party {
-   fn eq(
-      &self,
-      other: &Self,
-   ) -> bool {
-      // because `cake` is a single field/enum, we can derive `PartialEq` for it above
-      self.cake == other.cake
-   }
-}
+use traits::{admire_cake, smell_cake, Cake, Party};
 
 fn main() {
    // 1. The code below doesn't work because Cake doesn't implement Debug.
@@ -129,12 +87,4 @@ fn main() {
    // this is not needed, per RustRover, because:
    // "invoking `std::mem::drop` with a value that implements `Copy` does nothing"
    // drop(cake);
-}
-
-pub fn admire_cake(cake: Cake) {
-   println!("What a nice {:#?} cake! 🎂", cake);
-}
-
-pub fn smell_cake<T: Into<Cake>>(something: T) {
-   println!("Hmm...something smells like a {:?} cake!", something.into());
 }
