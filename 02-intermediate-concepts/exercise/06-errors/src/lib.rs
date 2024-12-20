3777d19908a53e4bfcf9ce2775b7233e1d4cdca6
Metal Gabe <gfl.wrk@gmail.com>
2024-12-20 00:57:31 -0800

    chore(main): format exercise 6;

--- 02-intermediate-concepts/exercise/06-errors/src/lib.rs
+++ 02-intermediate-concepts/exercise/06-errors/src/lib.rs
@@ -17,31 +17,31 @@
 // pub enum DolphinError...
 
 pub struct Dolphin {
-    pub name: String,
-    pub age: u8,
-    pub hungry: bool,
+   pub name: String,
+   pub age: u8,
+   pub hungry: bool,
 }
 
 impl Dolphin {
-    pub fn say_your_name(&self) -> Result<String, DolphinError> {
-        if self.name.len() > 10 {
-            Err(DolphinError::LongName)
-        } else {
-            Ok(format!("Hi, my name is {} and I'm a Dolphin!", self.name))
-        }
-    }
-    pub fn flip(&self) -> Result<String, DolphinError> {
-        if self.age < 4 {
-            Err(DolphinError::TooYoung)
-        } else {
-            Ok(format!("Yippee, I'm doing a flip!"))
-        }
-    }
-    pub fn shake_hands(&self) -> Result<String, DolphinError> {
-        if self.hungry {
-            Err(DolphinError::Hungry)
-        } else {
-            Ok(format!("Nice to meet you, let's shake hands!"))
-        }
-    }
+   pub fn say_your_name(&self) -> Result<String, DolphinError> {
+      if self.name.len() > 10 {
+         Err(DolphinError::LongName)
+      } else {
+         Ok(format!("Hi, my name is {} and I'm a Dolphin!", self.name))
+      }
+   }
+   pub fn flip(&self) -> Result<String, DolphinError> {
+      if self.age < 4 {
+         Err(DolphinError::TooYoung)
+      } else {
+         Ok(format!("Yippee, I'm doing a flip!"))
+      }
+   }
+   pub fn shake_hands(&self) -> Result<String, DolphinError> {
+      if self.hungry {
+         Err(DolphinError::Hungry)
+      } else {
+         Ok(format!("Nice to meet you, let's shake hands!"))
+      }
+   }
 }
--- 02-intermediate-concepts/exercise/06-errors/src/main.rs
+++ 02-intermediate-concepts/exercise/06-errors/src/main.rs
@@ -27,44 +27,44 @@ use aquarium::Dolphin;
 // }
 
 fn main() {
-    let dolphins = vec![
-        Dolphin {
-            name: "Augustinius".into(),
-            age: 7,
-            hungry: false,
-        },
-        Dolphin {
-            name: "Bitty".into(),
-            age: 2,
-            hungry: true,
-        },
-        Dolphin {
-            name: "Carson".into(),
-            age: 5,
-            hungry: true,
-        },
-        Dolphin {
-            name: "Devin".into(),
-            age: 6,
-            hungry: false,
-        },
-    ];
-    for dolphin in &dolphins {
-        // Challenge: Change main() so that it returns a Result, and instead of handling the error
-        // that play_time returns, use the try (?) operator to only handle the success condition.
-        // 
-        // If done correctly, the output of the program will become much shorter. Since play_time
-        // returns an Err variant the first time it is called, the try operator will return it from
-        // main(), which will end the program at the first error. anyhow's Result will take care of
-        // formatting the error output for us.
-        match play_time(dolphin) {
-            Ok(responses) => {
-                println!("{} did a FABULOUS PERFORMANCE!", dolphin.name);
-                for response in responses {
-                    println!("  {}", response);
-                }
+   let dolphins = vec![
+      Dolphin {
+         name: "Augustinius".into(),
+         age: 7,
+         hungry: false,
+      },
+      Dolphin {
+         name: "Bitty".into(),
+         age: 2,
+         hungry: true,
+      },
+      Dolphin {
+         name: "Carson".into(),
+         age: 5,
+         hungry: true,
+      },
+      Dolphin {
+         name: "Devin".into(),
+         age: 6,
+         hungry: false,
+      },
+   ];
+   for dolphin in &dolphins {
+      // Challenge: Change main() so that it returns a Result, and instead of handling the error
+      // that play_time returns, use the try (?) operator to only handle the success condition.
+      //
+      // If done correctly, the output of the program will become much shorter. Since play_time
+      // returns an Err variant the first time it is called, the try operator will return it from
+      // main(), which will end the program at the first error. anyhow's Result will take care of
+      // formatting the error output for us.
+      match play_time(dolphin) {
+         Ok(responses) => {
+            println!("{} did a FABULOUS PERFORMANCE!", dolphin.name);
+            for response in responses {
+               println!("  {}", response);
             }
-            Err(e) => println!("{} can't perform today: {}", dolphin.name, e.to_string()),
-        }
-    }
+         },
+         Err(e) => println!("{} can't perform today: {}", dolphin.name, e.to_string()),
+      }
+   }
 }
