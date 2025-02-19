82a213d984a3dd092f63a51a09f9965bfe4a8205
Metal Gabe <gfl.wrk@gmail.com>
2025-02-18 22:29:46 -0800

    chore(main): complete exercise 8-logging;

--- 02-intermediate-concepts/exercise/08-logging/src/lib.rs
+++ 02-intermediate-concepts/exercise/08-logging/src/lib.rs
@@ -1,45 +1,52 @@
 // 1. Bring the macros `error, warn, info, debug, trace` into scope from the log package with a
 // `use` statement.
-//
 // You should be able to run `cargo build --lib` successfully after this step (and each step in this
 // file)
 //
 // Hint: You need to update Cargo.toml to add the `log` dependency, first.
+use log::{debug, error, info, trace, warn};
 
 #[derive(Debug)]
 pub struct Frog {
-    energy: u8,
-    sleeping: bool,
+   energy: u8,
+   sleeping: bool,
 }
 
 impl Frog {
-    pub fn new() -> Self {
-        // 2. Use debug!() to log "A new Frog has been created"
-        Default::default()
-    }
-    pub fn hop(&mut self) {
-        self.energy -= 1;
-        // 3. Use info!() to log that a Frog hopped, and how much energy is left
-        if self.energy == 0 {
-            // 4. Use warn!() to warn that the frog will go to sleep since he ran out of energy
-            self.sleep();
-        }
-    }
-    pub fn sleep(&mut self) {
-        if self.sleeping {
-            // 5. Use error!() to log a (non-fatal) error stating that the Frog is already asleep
-        } else {
-            self.sleeping = true;
-        }
-    }
+   pub fn new() -> Self {
+      // 2. Use debug!() to log "A new Frog has been created"
+      debug!(target: "\"impl Frog\"", "A new Frog has been created");
+      Default::default()
+   }
+   pub fn hop(&mut self) {
+      self.energy -= 1;
+      // 3. Use info!() to log that a Frog hopped, and how much energy is left
+      info!(target: "\"hop\"", "Hop! Energy left: {}", self.energy);
+      if self.energy == 0 {
+         warn!(target: "\"hop if\"", "Hop! Hop! I'm out of energy! Going sleepy time now...");
+         // 4. Use warn!() to warn that the frog will go to sleep since he ran out of energy
+         self.sleep();
+      }
+   }
+   pub fn sleep(&mut self) {
+      if self.sleeping {
+         // 5. Use error!() to log a (non-fatal) error stating that the Frog is already asleep
+         error!(target: "\"sleep\"", "Hop! Hop! I'm already asleep!");
+      } else {
+         self.sleeping = true;
+      }
+   }
 }
 
 impl Default for Frog {
-    fn default() -> Self {
-        // 6. Use trace!() to log that a default value was generated, with the debug representation
-        Frog {
-            energy: 5,
-            sleeping: false,
-        }
-    }
+   fn default() -> Self {
+      // 6. Use trace!() to log that a default value was generated, with the debug representation
+      let frog = Frog {
+         energy: 5,
+         sleeping: false,
+      };
+
+      trace!(target: "\"default\"", "Default Frog created: {:?}", frog);
+      frog
+   }
 }
--- 02-intermediate-concepts/exercise/08-logging/src/main.rs
+++ 02-intermediate-concepts/exercise/08-logging/src/main.rs
@@ -7,20 +7,21 @@ use frogger::Frog;
 // 7. Update Cargo.toml to add the `env_logger` dependency
 
 fn main() {
-    // 8. Initialize env_logger using the init() function at the top level of the library
+   // 8. Initialize env_logger using the init() function at the top level of the library
+   env_logger::init();
 
-    // 9. Run this program with `cargo run` and take a look at the default output.
-    // - Now run it again with an explicit log level, like `RUST_LOG=info cargo run`
-    // - or `RUST_LOG=trace cargo run`
-    let mut skippy = Frog::new();
-    skippy.hop();
-    skippy.hop();
-    skippy.hop();
-    skippy.hop();
-    skippy.hop();
-    skippy.sleep();
-    skippy.sleep();
+   // 9. Run this program with `cargo run` and take a look at the default output.
+   // - Now run it again with an explicit log level, like `RUST_LOG=info cargo run`
+   // - or `RUST_LOG=trace cargo run`
+   let mut skippy = Frog::new();
+   skippy.hop();
+   skippy.hop();
+   skippy.hop();
+   skippy.hop();
+   skippy.hop();
+   skippy.sleep();
+   skippy.sleep();
 
-    // Challenge: Go back to lib.rs and set the `target: ` argument for each logging call to be the
-    // path to the function.  For example, `Frog::new`
+   // Challenge: Go back to lib.rs and set the `target: ` argument for each logging call to be the
+   // path to the function.  For example, `Frog::new`
 }
