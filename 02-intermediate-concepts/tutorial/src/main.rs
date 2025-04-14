ddf9a8ad876a5c6ea7fd95f93f8c5fd1939686f0
Metal Gabe <gfl.wrk@gmail.com>
2025-04-13 23:25:26 -0700

    chore(main): add player sprite to `tutorial` lesson;

--- 02-intermediate-concepts/tutorial/src/main.rs
+++ 02-intermediate-concepts/tutorial/src/main.rs
@@ -12,16 +12,25 @@ impl Default for GameState {
    fn default() -> Self {
       Self {
          current_score: 0,
-         enemy_labels: 0,
-         high_score: Vec::new(),
-         spawn_timer: Timer::from_seconds(1.0, false),
+         enemy_labels: Vec::new(),
+         high_score: 0,
+         spawn_timer: Timer::from_seconds(1.0, TimerMode::Repeating),
       }
    }
 }
 
 fn main() {
    let mut game = Game::new();
-   // set up game stuff here
-   let initial_state = GameState { score: 0 };
+   let player = game.add_sprite("player", SpritePreset::RacingCarRed);
+   player.translation = Vec2::new(200.0, 100.0);
+   player.rotation = std::f32::consts::FRAC_PI_2;
+   player.scale = 0.5;
+   let initial_state = GameState::default();
+   game.add_logic(game_logic);
    game.run(initial_state);
+}
+
+fn game_logic(engine: &mut Engine, state: &mut GameState) {
+   // state.current_score += 1;
+   // println!("Score: {}", state.current_score);
 }
