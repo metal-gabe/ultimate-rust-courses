87671ffd11cbb4e8f6102a188f9294aa41c66770
Metal Gabe <gfl.wrk@gmail.com>
2025-04-17 20:30:59 -0700

    chore(main): add collider detection and event handling logic;

--- 02-intermediate-concepts/tutorial/src/main.rs
+++ 02-intermediate-concepts/tutorial/src/main.rs
@@ -21,16 +21,42 @@ impl Default for GameState {
 
 fn main() {
    let mut game = Game::new();
+
    let player = game.add_sprite("player", SpritePreset::RacingCarRed);
-   player.translation = Vec2::new(200.0, 100.0);
+   player.translation = Vec2::new(100.0, 100.0);
    player.rotation = std::f32::consts::FRAC_PI_2;
    player.scale = 0.5;
+   player.collision = true;
+
+   let car1 = game.add_sprite("car1", SpritePreset::RacingCarBlue);
+   car1.translation = Vec2::new(300.0, 100.0);
+   // car1.rotation = std::f32::consts::FRAC_PI_2;
+   car1.scale = 0.5;
+   car1.collision = true;
+
    let initial_state = GameState::default();
    game.add_logic(game_logic);
    game.run(initial_state);
 }
 
-fn game_logic(engine: &mut Engine, state: &mut GameState) {
-   // state.current_score += 1;
-   // println!("Score: {}", state.current_score);
+fn game_logic(
+   engine: &mut Engine,
+   state: &mut GameState,
+) {
+   for event in engine.collision_events.drain(..) {
+      if event.state == CollisionState::Begin && event.pair.one_starts_with("player") {
+         // remove the sprite the player collided with
+         for label in [event.pair.0, event.pair.1] {
+            if label != "player" {
+               engine.sprites.remove(&label);
+            }
+         }
+
+         state.current_score += 1;
+         println!("Score: {}", state.current_score);
+      }
+   }
+
+   let player = engine.sprites.get_mut("player").unwrap();
+   player.translation.x += 100.0 * engine.delta_f32;
 }
